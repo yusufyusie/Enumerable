560c5aa4f72ecd34c0730223b9444ac9d31b4248
yusufyusie <yesuf023@gmail.com>
2023-10-14 07:25:12 +0300

    Add MyList class

---
+++ my_list.rb
@@ -0,0 +1,24 @@
+class Mylist
+  include MyEnumerable
+
+  def initialize(*args)
+    @list = args
+  end
+
+  def each(&block)
+    @list.each(&block)
+  end
+end
+
+new_list = Mylist.new([1, 2, 3, 4]) # true
+
+# Test #all?
+puts(new_list.all? { |e| e < 5 })
+puts(new_list.all? { |e| e > 5 })
+
+# Test #any?
+puts(new_list.any? { |e| e == 2 })
+puts(new_list.any? { |e| e == 5 })
+
+# Test #filter
+puts(new_list.filter(&:even?))
